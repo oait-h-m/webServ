1ccf55867923d20ca6106bb6cc2051090ee113b6
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-06-08 19:49:11 +0100

    implement getbody and test it

--- httpRequest/httpRequestParser.cpp
+++ httpRequest/httpRequestParser.cpp
@@ -1,5 +1,17 @@
 #include "httpRequestParser.hpp"
 
+static std::string getFirstLine(const std::string& rawRequest, size_t &pos)
+{
+    pos = rawRequest.find("\r\n");
+
+    if (pos == std::string::npos)
+        return rawRequest;
+
+    return rawRequest.substr(0, pos);
+}
+
+
+
 const std::string& httpRequest::getMethod() const
 {
     return method;
@@ -14,6 +26,11 @@ const std::string& httpRequest::getVersion() const
     return version;
 }
 
+
+const std::string& httpRequest::getBody() const
+{
+    return body;
+}
 
 void httpRequest::parse(const std::string& rawRequest)
 {
@@ -49,6 +66,20 @@ void httpRequest::parse(const std::string& rawRequest)
 
         headers[key] = value;
     }
+    size_t pos = rawRequest.find("\r\n\r\n");
+
+    if (pos != std::string::npos)
+        body = rawRequest.substr(pos + 4);
+}
+
+void httpRequest::printHeaders() const
+{
+    std::map<std::string, std::string>::const_iterator it;
+
+    for (it = headers.begin(); it != headers.end(); ++it)
+    {
+        std::cout << it->first << " => " << it->second << std::endl;
+    }
 }
 
 int main()
@@ -56,16 +87,20 @@ int main()
     httpRequest req;
 
     std::string rawRequest =
-        "GET /index.html HTTP/1.1\r\n"
-        "Host: localhost:8080\r\n"
-        "User-Agent: Mozilla/5.0\r\n"
-        "Content-Type: text/html\r\n"
-        "\r\n";
+    "POST /upload HTTP/1.1\r\n"
+    "Host: localhost\r\n"
+    "Content-Type: application/x-www-form-urlencoded\r\n"
+    "Content-Length: 27\r\n"
+    "\r\n"
+    "username=john&password=1234";
 
     req.parse(rawRequest);
 
     std::cout << "Method: " << req.getMethod() << std::endl;
     std::cout << "URI: " << req.getUri() << std::endl;
     std::cout << "Version: " << req.getVersion() << std::endl;
+    std::cout << "Body: " << req.getBody() << std::endl;
 
+    std::cout << "\nHeaders:\n";
+    req.printHeaders();
 }
