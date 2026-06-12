7cb2e91ec9a085801101d4ceb63e86dd5aa25edc
hmouis <houdayfamouis@gmail.com>
2026-06-12 17:53:23 +0100

    ADD Server header file

---
+++ src/Server/Server.hpp
@@ -0,0 +1,30 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string>
+#include <iostream>
+#include <vector>
+
+class Server {
+private:
+    int listening_fd;
+    int port;
+    std::string server_name;
+    std::string root_dir;
+    
+public:
+    Server(int port, const std::string &name, const std::string &root);
+    ~Server();
+    
+    void init();
+    int acceptClient();
+    int getListeningFd() ;
+    int getPort();
+    std::string getServerName() ;
+    std::string getRootDir() ;
+};
+
+#endif
