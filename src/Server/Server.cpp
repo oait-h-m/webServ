bab4b3baffe5be07ef187e8a4fb5b47e34224022
hmouis <houdayfamouis@gmail.com>
2026-06-12 17:51:25 +0100

    ADD init function

---
+++ src/Server/Server.cpp
@@ -0,0 +1,26 @@
+#include "Server.hpp"
+
+
+Server::Server(int port, const std::string &name, const std::string &root): port(port), server_name(name), root_dir(root)
+{}
+Server::~Server()
+{}
+
+void Server::init()
+{
+    listening_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening_fd < 0)
+        throw std::runtime_error("Error: socket failed");
+    
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listening_fd, (struct sockaddr *)&server, sizeof(server)))
+        throw std::runtime_error("Error: bind failed");
+
+    if (listen(listening_fd, 10000))
+        throw std::runtime_error("Error: listen failed");
+}
+
