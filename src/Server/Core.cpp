43bbaa8b2e013f030d0cce8d322b331400780d88
hmouis <houdayfamouis@gmail.com>
2026-06-12 18:43:45 +0100

    setup epoll to all servers

--- src/Server/Core.cpp
+++ src/Server/Core.cpp
@@ -10,8 +10,11 @@ Core::~Core()
 void Core::creatServers()
 {
     std::vector<int>::iterator p_it;
+    p_it = s_ports.begin();
     std::vector<std::string>::iterator n_it;
+    n_it = s_names.begin();
     std::vector<std::string>::iterator r_it;
+    r_it = s_roots.begin();
     for (int i = 0; i < n_servers; i++)
     {
         Server *server = new Server(*p_it, *n_it, *r_it);
@@ -19,5 +22,24 @@ void Core::creatServers()
         ++p_it;
         ++n_it;
         ++r_it;
+    }
+}
+
+void Core::setupEpoll()
+{
+    epoll_fd = epoll_create(1000);
+    if (epoll_fd < 0)
+        throw std::runtime_error("Error: epoll_create failed");
+    
+    std::vector<Server *>::iterator it;
+    it = servers.begin();
+    for (int i = 0; i < n_servers; i++)
+    {
+        struct epoll_event ev;
+        ev.events = EPOLLIN;
+        ev.data.fd = (*it)->getListeningFd();
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, (*it)->getListeningFd(), &ev))
+            throw std::runtime_error("ERROR: epoll_ctl failed");
+        ++it;
     }
 }
