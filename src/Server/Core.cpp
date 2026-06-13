42312f6f2c1adba0b37f8941b366102cc34ee761
hmouis <houdayfamouis@gmail.com>
2026-06-13 14:45:44 +0100

    ADD the implementation off the following functions: removeClient() isListeningFd() getServer() acceptClient()

--- src/Server/Core.cpp
+++ src/Server/Core.cpp
@@ -32,14 +32,104 @@ void Core::setupEpoll()
         throw std::runtime_error("Error: epoll_create failed");
     
     std::vector<Server *>::iterator it;
-    it = servers.begin();
-    for (int i = 0; i < n_servers; i++)
+    for (it = servers.begin(); it != servers.end() ; it++)
     {
         struct epoll_event ev;
         ev.events = EPOLLIN;
         ev.data.fd = (*it)->getListeningFd();
         if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, (*it)->getListeningFd(), &ev))
             throw std::runtime_error("ERROR: epoll_ctl failed");
-        ++it;
+    }
+}
+
+int Core::isListeningFd(int fd)
+{
+    std::vector<Server *>::iterator it;
+    for (it = servers.begin(); it != servers.end() ; it++)
+    {
+        if ((*it)->getListeningFd() == fd)
+            return 1;
+    }
+    return 0;
+}
+
+Server* Core::getServer(int fd)
+{
+    std::vector<Server *>::iterator it;
+    for (it = servers.begin(); it != servers.end() ; it++)
+    {
+        if ((*it)->getListeningFd() == fd)
+            return *it;
+    }
+    return NULL;
+}
+
+void Core::acceptClient(Server* server)
+{
+    struct sockaddr_in client_info;
+    socklen_t client_len = sizeof(client_info);
+
+    int client_fd = accept(server->getListeningFd(), (struct sockaddr *)&client_info, &client_len);
+    if (client_fd < 0)
+    {
+        std::cout << "ERROR: accept client faild for server:" << server->getListeningFd() << std::endl;
+        return ;
+    }
+    struct epoll_event client_ev;
+    client_ev.events = EPOLLIN;
+    client_ev.data.fd = client_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_ev) < 0)
+    {
+        std::cerr << "epoll_ctl ADD client failed for server:" << server->getListeningFd() << std::endl;
+        close(client_fd);
+        return ;
+    }
+    Client *client = new Client(client_fd, server);
+    all_clients[client_fd] = client;
+}
+
+void Core::removeClient(int fd)
+{
+    std::cout << "Client fd=" << fd << " disconnected" << std::endl;
+
+    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+    close(fd);
+}
+
+void Core::run()
+{
+    creatServers();
+
+    std::vector<Server *>::iterator it;
+    for (it = servers.begin(); it != servers.end(); it++)
+        (*it)->init();
+    
+    setupEpoll();
+
+    struct epoll_event events[10000];
+    while (true)
+    {
+        int ready = epoll_wait(epoll_fd, events, 10000, -1);
+        if (ready < 0){
+            std::cout << "ERROR: epoll_wait failed\n";
+            break;
+        }
+        for (int i = 0; i < ready; i++)
+        {
+            int fd = events[i].data.fd;
+            uint32_t event_mask = events[i].events;
+
+            if (isListeningFd(fd))
+            {
+                acceptClient(getServer(fd));
+                continue;
+            }
+        }
+        
+
+
+
+
+
     }
 }
