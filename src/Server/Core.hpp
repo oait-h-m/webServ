e8c1a3ea3b12d14218cff4a8fcfd8178fa481c71
hmouis <houdayfamouis@gmail.com>
2026-06-13 14:23:38 +0100

    ADD the following functions to the Core class: getServer that return which Server that the client connected to and the isListeningFd that return Success when the fd is one of the servers listening fd

--- src/Server/Core.hpp
+++ src/Server/Core.hpp
@@ -6,6 +6,7 @@
 #include "Server.hpp"
 #include "Client.hpp"
 #include <sys/epoll.h>
+#include <unistd.h>
 
 
 class Core {
@@ -19,15 +20,16 @@ private:
     std::map<int, Client*> all_clients;
     
     void setupEpoll();
+    int  isListeningFd(int fd);
+    Server* getServer(int fd);
     void creatServers();
-    void acceptClient(Server *server, int listening_fd);
+    void  acceptClient(Server *server);
     void removeClient(int fd);
     
 public:
     Core();
     ~Core();
     
-    void addServer(Server *server);
     void run(); 
 };
 
