f5bda360dbbd7bcf19b7b49ba494be5b32c26e72
sboukiou <sboukiou@student.1337.ma>
2026-06-17 06:03:02 +0100

    Added more needed member methods + updating the construction to comply with OCF

--- src/config_file_parser/ConfigParser.hpp
+++ src/config_file_parser/ConfigParser.hpp
@@ -7,18 +7,19 @@
 class ConfigParser {
 	private:
 		std::vector<Token>	_tokens;
-		size_t				_current;
+		size_t				_pos;
 		WebServerConfig		_global_config;
 
 		void	_parse_server();
 		void	_parse_location(ServerConfig &server);
-		void	_parse_directive(LocationConfig &location);
+		void	_parse_location_directive(LocationConfig &lc_config);
+		void	_parse_server_directive(ServerConfig &sv_config);
 		const	Token	&_consume();
 		const	Token	&_peek() const;
-		void	_match(TokenType expected) const;
+		void	_match(TokenType expected);
 		ConfigParser();
 		ConfigParser(const ConfigParser &other);
-		void	operator=(const ConfigParser &other);
+		ConfigParser&	operator=(const ConfigParser &other);
 	public:
 		ConfigParser(const std::vector<Token> &tokens);
 		const WebServerConfig	&parse();
