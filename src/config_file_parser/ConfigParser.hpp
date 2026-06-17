0c73754ee21407327ab7de3836d0c8d9bb2b57c7
sboukiou <sboukiou@student.1337.ma>
2026-06-17 23:27:08 +0100

    Refactoring the ConfigParser:
    Defined helper methods for each server-specific directive handling to
    split the logic and ease the testing.
    each directive withing the server has its own mini-parser called from
    the main server directives parser.
    
    The server context draft is done and ready.

--- src/config_file_parser/ConfigParser.hpp
+++ src/config_file_parser/ConfigParser.hpp
@@ -15,6 +15,11 @@ class ConfigParser {
 		WebServerConfig		_global_config;
 
 		void	_parse_server();
+		void	_parse_listen_directive(ServerConfig &sv_config);
+		void	_parse_server_name_directive(ServerConfig &sv_config);
+		void	_parse_cmb_size_directive(ServerConfig &sv_config);
+		void	_parse_root_directive(ServerConfig &sv_config);
+		void	_parse_error_page_directive(ServerConfig &sv_config);
 		void	_parse_location(ServerConfig &server);
 		void	_parse_location_directive(LocationConfig &lc_config);
 		void	_validate_interface_port(std::string &input, ServerConfig &sv_config);
