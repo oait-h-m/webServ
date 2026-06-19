7d1a6c6471240ac9a0daba2112f02d39cbac40c4
Said Boukioud <said.boukioud@zohomail.com>
2026-06-19 12:15:21 +0100

    UPDATE: Made the default constructor public

--- src/config_file_parser/ConfigParser.hpp
+++ src/config_file_parser/ConfigParser.hpp
@@ -27,10 +27,10 @@ class ConfigParser {
 		const	Token	&_consume();
 		const	Token	&_peek() const;
 		void	_match(TokenType expected);
-		ConfigParser();
 		ConfigParser(const ConfigParser &other);
 		ConfigParser&	operator=(const ConfigParser &other);
 	public:
+		ConfigParser();
 		ConfigParser(const std::vector<Token> &tokens);
 		void	parse();
 		WebServerConfig generate_config(std::string &file_path);
