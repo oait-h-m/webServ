bdeaaf0eafe326745f490fbc49f0dbdd0056c7bd
sboukiou <sboukiou@student.1337.ma>
2026-06-20 20:26:46 +0100

    REFACTOR:
    Redefined the location/server directive dispatcher methods --that are
    common (server_name, error_page, client_max_body_size)-- as template
    methods to comply with DRY.
    
    ADD:
    
    Implementing the rest of the parser ( Location config directives)
    including: allowed_methods, autoindex

--- src/config_file_parser/InternalConfigs.hpp
+++ src/config_file_parser/InternalConfigs.hpp
@@ -10,14 +10,18 @@ struct LocationConfig {
 	std::string root;
 	std::vector<std::string> index_files;
 	std::vector<std::string> allowed_methods;
+	size_t client_max_body_size;
 	bool autoindex;
 	std::string upload_path;
 	std::string cgi_extension;
 	std::string cgi_path;
 	std::string redirection;
+	std::map<int, std::string> error_pages;
 	LocationConfig() {
 		index_files.clear();
 		allowed_methods.clear();
+		error_pages.clear();
+		client_max_body_size = 0;
 		autoindex = false;
 	}
 
