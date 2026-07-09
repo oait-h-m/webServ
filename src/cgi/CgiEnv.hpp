0374c2faded579710e914cc8ec0cc2156654d1e7
Said <sboukiou@student.1337.ma>
2026-07-09 12:34:27 +0100

    Defining the class for CgiEnv

--- src/cgi/CgiEnv.hpp
+++ src/cgi/CgiEnv.hpp
@@ -1,10 +1,22 @@
 #ifndef CGIENV_HPP
 # define CGIENV_HPP
 
+#include "../config_file_parser/InternalConfigs.hpp"
+#include "../request_parser/httpRequestParser.hpp"
+
 class CgiEnv {
 	private:
-		char **envp; /* The container for Environment variables */
+		char **_envp; /* The container for Environment variables */
+		LocationConfig &_lc_conf;
+		HttpRequest		&_request;
+		CgiEnv();
+		CgiEnv(const CgiEnv &other);
+		CgiEnv &operator=(const CgiEnv &other);
+		void	_build_env();
 	public:
+		CgiEnv(LocationConfig &conf, HttpRequest &req);
+		~CgiEnv();
+		char **get_envp() const;
 
 };
 
